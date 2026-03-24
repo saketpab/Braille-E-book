f870dbf05baaea153a2e6089c89b76e2e4be5948
saketpab <saketp73@gmail.com>
2026-03-24 19:04:40 -0400

    barille translator

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Braille-E-book
 Software for the braille E-book
+
+How to use/implement this code
+1. Clone this repo (git clone ...)
+2. run cd english2braille-api
+3. Create a virtual environment in english2braille-api folder in terminal: python3 -m venv venv
+4. Run the virtual environment
+    Windows: venv\Scripts\activate
+    Mac: source venv/bin/activate
+5. Run the requirments.txt file: pip install -r requirements.txt
+4. run the input.py file: python3 input.py and insert your word
+5. You should then see the output in the correct format (braille and matrix)
--- input.py
+++ input.py
@@ -1,3 +1,5 @@
+import requests
+
 def getWord():
 
     wordSizeLimit = 1
@@ -12,8 +14,39 @@ def getWord():
     
     return user_input
 
+def send_to_api(word):
+    res = requests.post(
+        "http://localhost:5000/translate",
+        json={"translate": word}
+    )
+    
+    return res.json()
+
+def braille_to_matrix(char):
+    code = ord(char) - 0x2800  # Braille Unicode starts here
+    
+    # Each bit corresponds to a dot
+    dots = [
+        (code >> 0) & 1,  # dot 1
+        (code >> 1) & 1,  # dot 2
+        (code >> 2) & 1,  # dot 3
+        (code >> 3) & 1,  # dot 4
+        (code >> 4) & 1,  # dot 5
+        (code >> 5) & 1,  # dot 6
+    ]
+    
+    return [
+        [dots[0], dots[3]],
+        [dots[1], dots[4]],
+        [dots[2], dots[5]],
+    ]
+
 
 
 if __name__ == "__main__":
     theWord = getWord()
-    print(f"You entered: {theWord}")
+    #print(f"You entered: {theWord}")
+    result = send_to_api(theWord)
+    matrix = braille_to_matrix(result['response'])
+    print(f"API Response: {result}")
+    print(f"Braille Matrix: {matrix}")
